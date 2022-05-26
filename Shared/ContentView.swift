6e40c4f28134e0614319cfde16dcb8f00ddfae84
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-05-26 12:23:24 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Shounak Ghosh on 5/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/intro_ios_appdevApp.swift
@@ -0,0 +1,17 @@
+//
+//  intro_ios_appdevApp.swift
+//  Shared
+//
+//  Created by Shounak Ghosh on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct intro_ios_appdevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
